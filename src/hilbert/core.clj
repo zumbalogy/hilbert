bb353522b90eeeacb970955bc90b8deecebbd5da
zumbalogy <intrapostal@gmail.com>
2016-12-04 20:51:23 +0700

    super rainbow fish

--- src/hilbert/core.clj
+++ src/hilbert/core.clj
@@ -21,22 +21,21 @@
             x (mod i 300)
             y (quot i 300)
             h (hilbert->int [x y])
-            [x2 y2] (int->hilbert (* h 2) 2)
+            [x2 y2] (int->hilbert (* h 1) 2)
             h2 (+ x2 (* 300 y2))
-            p2 (get pixels (mod h2 (* 300 300)))]
-            ; p2 100]
-            ; [r g b] (rgb p)
-            ; h (hilbert->int [r g b])
-            ; h2 (+ h x)
-            ; [r2 g2 b2] (int->hilbert h2 3)
-            ; p2 (component [r2 g2 b2])]
-        (aset pixels i p2)))
+            p2 (get pixels (mod h2 (* 300 300)))
+            [r g b] (rgb p2)
+            h3 (hilbert->int [r g b])
+            h4 (* x2 (/ h3 (inc y2)))
+            [r2 g2 b2] (int->hilbert h4 3)
+            p3 (component [r2 g2 b2])]
+        (aset pixels i p3)))
     (img/set-pixels fish pixels)
     (show fish)
     (img/save fish (str "output/" (System/currentTimeMillis) ".png"))))
 
 (defn -main []
-  (make-fish 200))
-  ; (doseq [i (range 10 1200 13)]
+  (make-fish 1))
+  ; (doseq [i (range 1 1.1 0.002)]
   ;   (println i)
   ;   (make-fish i)))
