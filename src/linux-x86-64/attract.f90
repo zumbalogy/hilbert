cbf2e17eb778f148848558ca3dbe5a0dc2fb6ff7
zumbalogy <intrapostal@gmail.com>
2017-11-11 23:10:46 -0500

    cool attractors

--- src/linux-x86-64/attract.f90
+++ src/linux-x86-64/attract.f90
@@ -3,7 +3,7 @@ subroutine test(pic) bind(c)
   implicit none
 
   ! integer :: pic(1920 * 1282)
-  integer :: pic(300 * 300)
+  integer :: pic(3540 * 3540)
   integer :: pic_size, pic_width, pic_height
   integer :: i
   integer :: r, g, b, r2, g2, b2
@@ -13,20 +13,20 @@ subroutine test(pic) bind(c)
   real :: j, k, j2, k2, j_index, k_index
 
 
-  w = 1.533
-  x = 11.8
-  y = 1.62
-  z = 0.2
+  w =  -1.7
+  x = 1.8
+  y = -1.9
+  z =-0.4
 
 
   j = 0.0
   k = 0.0
 
-  j_scale = 50
-  k_scale = 50
+  j_scale = 600
+  k_scale = 600
 
-  pic_width = 300
-  pic_height = 300
+  pic_width = 3540
+  pic_height = 3540
 
   pic_size = size(pic)
 
@@ -34,7 +34,7 @@ subroutine test(pic) bind(c)
      pic(i) = -16777216
   end do
 
-  do i = 1, 5000000
+  do i = 1, 160000000
 
      j2 = sin(real(w * k)) + (y * cos(real(w * j)))
      k2 = sin(real(y * j)) + (z * cos(real(y * k)))
@@ -48,11 +48,41 @@ subroutine test(pic) bind(c)
      k_index = int(k_index * k_scale)
 
 
-     index = j_index + (k_index * 300)
+     index = j_index + (k_index * pic_width)
 
      if (index > 0) then
         if (index < pic_size) then
-           pic(index) = min(pic(index) + 40, 0)
+
+           p = pic(index)
+
+           ! 0x00ff0000 == 16711680
+           r = rshift(iand(p, 16711680), 16)
+           ! 0x0000ff00 == 65280
+           g = rshift(iand(p, 65280), 8)
+           ! 0x000000ff == 225
+           b = iand(p, 255)
+
+           !print*, r
+
+           if (mod(i, 3) == 0) then
+              r = min(255, r + 1)
+           else if (mod(i, 3) == 1) then
+              if (k2 > k) then
+                 g = min(255, g + 1)
+              end if
+           else
+              if (j2 > j) then
+                 b = min(255, b + 1)
+              end if
+           end if
+
+           ! -16777216 = -1000000 hex
+           p2 = -16777216
+           p2 = ior(p2, lshift(iand(r, 255), 16))
+           p2 = ior(p2, lshift(iand(g, 255), 8))
+           p2 = ior(p2, iand(b, 255))
+
+           pic(index) = p2
         end if
      end if
 
@@ -62,23 +92,7 @@ subroutine test(pic) bind(c)
      k = k2
      ! p = pic(i)
 
-     ! ! 0x00ff0000 == 16711680
-     ! r = rshift(iand(p, 16711680), 16)
-     ! ! 0x0000ff00 == 65280
-     ! g = rshift(iand(p, 65280), 8)
-     ! ! 0x000000ff == 225
-     ! b = iand(p, 255)
 
-
-
-
-     ! ! -16777216 = -1000000 hex
-     ! p2 = -16777216
-     ! p2 = ior(p2, lshift(iand(r, 255), 16))
-     ! p2 = ior(p2, lshift(iand(g, 255), 8))
-     ! p2 = ior(p2, iand(b, 255))
-
-     ! pic(i) = p2
   end do
 
 end subroutine test
